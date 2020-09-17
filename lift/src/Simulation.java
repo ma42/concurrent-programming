99ec929a5c8bcf6af404ad0bdc1cedf2ba64aec0
Matilda Andersson <ma4748an-s@student.lu.se>
2020-09-17 16:18:16 +0200

    First draft of lab 2

---
+++ lift/src/Simulation.java
@@ -0,0 +1,33 @@
+import lift.LiftView;
+import lift.Monitor;
+import lift.Person;
+import lift.Lift;
+
+public class Simulation {
+	private static final int NBR_PERSONS = 20;
+	
+	public static void main(String[] args) {
+        LiftView view = new LiftView();
+        Monitor monitor = new Monitor();
+
+        Person[] persons = new Person[NBR_PERSONS];
+
+        for(int i = 0; i < NBR_PERSONS; i++) {
+            Person person = new Person(view.createPassenger(), monitor);
+            persons[i] = person;
+            person.start();
+        }
+        
+        Lift lift = new Lift(view, monitor, persons);
+        lift.start();
+        
+        while(true) {
+        	for(int i = 0; i < NBR_PERSONS; i++) {
+        		if(!persons[i].isAlive()) {
+            		persons[i] = new Person(view.createPassenger(), monitor);
+            		persons[i].start();
+        		}
+        	}
+        }
+    }
+}
